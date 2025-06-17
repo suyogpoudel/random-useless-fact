c95885663a94f01ef884b6572ab8325cf2e679f7
Suyog Poudel <suyogpoudel583@gmail.com>
2025-06-17 11:55:52 +0545

    responsive

--- src/components/Fact.jsx
+++ src/components/Fact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Fact = ({randomFact}) => {
     return (
         <div className='mt-7 flex justify-center items-center text-[30px]'>
-            {Object.entries(randomFact).length === 0 ? <p>Loading...</p> : <div className='text-[40px] border-2 dark:border-slate-300 border-slate-900 w-[50%] max-sm:w-[90%] py-4 px-8 rounded-lg'>{randomFact.text}</div>}
+            {Object.entries(randomFact).length === 0 ? <p>Loading...</p> : <div className='text-[40px] max-sm:text-[35px] border-2 dark:border-slate-300 border-slate-900 w-[50%] max-sm:w-[90%] py-4 px-8 rounded-lg'>{randomFact.text}</div>}
         </div>
     )
 }
